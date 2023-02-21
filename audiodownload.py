927d95d6c3453b5e216ac9e8e70027b5c5435c9d
Douglas Freitas Lima da Silva <dodaflima@gmail.com>
2023-02-21 09:09:30 -0300

    Modificações necessárias

--- audiodownload.py
+++ audiodownload.py
@@ -17,34 +17,47 @@ import sys
 import os
 from requests import get
 
-def download(arquivo, idioma):
+def __save_audio(audio: bytes, name: str) -> None:
+    if not os.path.exists("audio/"):
+        try:
+            os.mkdir("audio")
+        except Exception as e:
+            raise Exception("Não foi possível criar a pasta de destino")
+    
+    save = open(f"audio/{name}.mp3", "wb")
+    save.write(audio)
+    save.close()
+
+def __download(word: str, lang: str) -> bytes:
     '''
     :param arquivo: string - Lista de palavras para serem baixadas
     :idioma idioma: en, pt-br, la, ja, ko ... sigla do idioma a ser baixado
+    :return: conteudo em bytes
     '''
-
-    with open(arquivo) as file:
-        palavras = [word.rstrip("\n") for word in file.readlines()]
-
-        for palavra in palavras:
-            url = f"https://translate.google.com/translate_tts?ie=UTF-8&q={palavra}&tl={idioma}&total=1&idx=0&tk=350535.255567&client=webapp&prev=input"
-            audio = get(url)
-            
-            if audio.status_code != 200:
-                print(f"Erro ao baixar palavra {palavra}: code {audio.status_code}")
-                raise Exception
-
-            if not os.path.exists("audio/"):
-                os.mkdir("audio")
-            
-            save = open(f"audio/{palavra}.mp3", "wb")
-            save.write(audio.content)
-            save.close()
+    url = f"https://translate.google.com/translate_tts?ie=UTF-8&q={word}&tl={lang}&total=1&idx=0&tk=350535.255567&client=webapp&prev=input"
+    audio = get(url)
     
+    if audio.status_code != 200:
+        raise Exception(f"Erro ao transferir a palavra {word}: status_code {audio.status_code}")
+    else:
+        return audio.content
+
+def download_from_list(lista: list, lang: str):
+    for word in lista:
+        audio = __download(word, lang)
+        __save_audio(audio, word)
+
+def download_from_arquive(arquive: str, lang: str):
+    if not os.path.exists(arquive):
+        raise Exception(f"Não foi possível abrir: {arquive}")
+
+    with open(arquive, "r") as file:
+        word_list = list((word.rstrip("\n") for word in file.readlines()))
+        download_from_list(word_list, lang)
+
 if __name__ == "__main__":
     try:
-        download(sys.argv[1], sys.argv[2])
-        
+        download_from_arquive(sys.argv[1], sys.argv[2])
     except Exception as e:
         print(e)
         print(__doc__)
